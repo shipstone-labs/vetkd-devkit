ac127b45e41474593e9e5ca4ec9b80c06e59a482
Jack Lloyd <jack.lloyd@dfinity.org>
2025-02-19 12:56:02 -0500

    Address some review comments

--- sdk/ic_vetkd_sdk_utils/test/index.test.ts
+++ sdk/ic_vetkd_sdk_utils/test/index.test.ts
@@ -1,5 +1,5 @@
-import { DerivedPublicKey, EncryptedKey, IdentityBasedEncryptionCiphertext, TransportSecretKey, VetKD, augmentedHashToG1, hashToScalar, deriveSymmetricKey } from "../src/index";
-import { assert, test } from 'vitest'
+import { DerivedPublicKey, EncryptedKey, IdentityBasedEncryptionCiphertext, TransportSecretKey, augmentedHashToG1, hashToScalar, deriveSymmetricKey } from "../src/index";
+import { expect, test } from '@jest/globals';
 
 function hexToBytes(hex: string): Uint8Array {
     const bytes = new Uint8Array(hex.length / 2);
@@ -15,14 +15,18 @@ function bytesToHex(bytes: Uint8Array): string {
         .join ("");
 }
 
-test('creating random TransportSecretKey', async () => {
+function assertEqual(a, b) {
+    expect(a).toStrictEqual(b);
+}
+
+test('creating random TransportSecretKey', () => {
     let key = TransportSecretKey.random();
 
     let pk = key.publicKeyBytes();
-    assert.deepEqual(pk.length, 48);
+    assertEqual(pk.length, 48);
 });
 
-test('parsing DerivedPublicKey', async () => {
+test('parsing DerivedPublicKey', () => {
     try {
         const invalid = new Uint8Array([1, 2, 3]);
         let key = new DerivedPublicKey(invalid);
@@ -31,10 +35,10 @@ test('parsing DerivedPublicKey', async () => {
 
     const valid = hexToBytes("972c4c6cc184b56121a1d27ef1ca3a2334d1a51be93573bd18e168f78f8fe15ce44fb029ffe8e9c3ee6bea2660f4f35e0774a35a80d6236c050fd8f831475b5e145116d3e83d26c533545f64b08464e4bcc755f990a381efa89804212d4eef5f");
     let key = new DerivedPublicKey(valid);
-    assert.deepEqual(valid, key.publicKeyBytes());
+    assertEqual(valid, key.publicKeyBytes());
 });
 
-test('augmented hash to G1', async() => {
+test('augmented hash to G1', () => {
     const pk = new DerivedPublicKey(hexToBytes("80e38f040fae321c75cf8faf8c6e9500c92b7cac022ca3eb48fb01c8e91d8c2bc806c2665ed28a0a8c87a4bff717dd3c0c4eb57ad635bc582f89c171b8478f2fe1b806c3faeed7133b13141aaf4a65aa0c5d7902dc80102e91e6f73fe56fa34f"));
     const msg = hexToBytes("25138dfc69267bd861d8ad9f05b9");
 
@@ -43,12 +47,12 @@ test('augmented hash to G1', async() => {
     const calculated = augmentedHashToG1(pk, msg);
 });
 
-test('protocol flow with precomputed data', async() => {
+test('protocol flow with precomputed data', () => {
     const tsk = new TransportSecretKey(hexToBytes("167b736e44a1c134bd46ca834220c75c186768612568ac264a01554c46633e76"));
 
     const tpk = tsk.publicKeyBytes();
 
-    assert.deepEqual(bytesToHex(tpk), "911969d56f42875d37a92d7eaa5d43293eff9f9a20ba4c60523e70a695eaeadeb721659b52a49d74e67841ad19033a12");
+    assertEqual(bytesToHex(tpk), "911969d56f42875d37a92d7eaa5d43293eff9f9a20ba4c60523e70a695eaeadeb721659b52a49d74e67841ad19033a12");
 
     const did = hexToBytes("6d657373616765");
 
@@ -58,11 +62,11 @@ test('protocol flow with precomputed data', async() => {
 
     const vetkd = ek.decryptAndVerify(tsk, dpk, did);
 
-    assert.deepEqual(bytesToHex(vetkd.signatureBytes()),
+    assertEqual(bytesToHex(vetkd.signatureBytes()),
                      "987db5406ce297e729c8564a106dc896943b00216a095fe9c5d32a16a330c02eb80e6f468ede83cde5462b5145b58f65");
 
     const symKey = vetkd.deriveSymmetricKey("QUUX-V01-CS02-with-expander-SHA256-128", 32);
-    assert.deepEqual(bytesToHex(symKey), "4e91306f72d20c58379ddf86e40869706c8c533d40c277cd808abaf6ac623fc1");
+    assertEqual(bytesToHex(symKey), "4e91306f72d20c58379ddf86e40869706c8c533d40c277cd808abaf6ac623fc1");
 
     const message = hexToBytes("f00f11");
     const seed = new Uint8Array(32);
@@ -71,17 +75,17 @@ test('protocol flow with precomputed data', async() => {
     const ibeRec = IdentityBasedEncryptionCiphertext.deserialize(ibe.serialize());
 
     const rec = ibeRec.decrypt(vetkd);
-    assert.deepEqual(bytesToHex(rec), "f00f11");
+    assertEqual(bytesToHex(rec), "f00f11");
 });
 
-test('hash to scalar', async() => {
+test('hash to scalar', () => {
     const dst = "QUUX-V01-CS02-with-BLS12381SCALAR_XMD:SHA-256_SSWU_RO_";
 
-    assert.deepEqual(hashToScalar(hexToBytes(""), dst).toString(16), "3b3fdf74b194c0a0f683d67a312a4e72d663d74b8478dc7b56be41e0ce11caa1");
-    assert.deepEqual(hashToScalar(hexToBytes("616263"), dst).toString(16), "47e7a8839695a3df27f202cf71e295a8554b47cef75c1e316b1865317720e188");
+    assertEqual(hashToScalar(hexToBytes(""), dst).toString(16), "3b3fdf74b194c0a0f683d67a312a4e72d663d74b8478dc7b56be41e0ce11caa1");
+    assertEqual(hashToScalar(hexToBytes("616263"), dst).toString(16), "47e7a8839695a3df27f202cf71e295a8554b47cef75c1e316b1865317720e188");
 });
 
-test('xmd test vectors', async() => {
+test('xmd test vectors', () => {
     const dst = "QUUX-V01-CS02-with-expander-SHA256-128";
     const testVectors = [
         ["", "68a985b87eb6b46952128911f2a4412bbc302a9d759667f87f7a21d803f07235"],
@@ -98,6 +102,6 @@ test('xmd test vectors', async() => {
         const outputLen = Math.trunc(expected.length / 2);
 
         const xmd = deriveSymmetricKey(input, dst, outputLen);
-        assert.deepEqual(bytesToHex(xmd), expected);
+        assertEqual(bytesToHex(xmd), expected);
     }
 });
