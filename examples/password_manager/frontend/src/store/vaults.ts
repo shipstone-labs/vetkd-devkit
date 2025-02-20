84fc218453b00a5824658e879c9f84a665b53458
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-02-20 11:18:15 +0100

    replace 2 more var's with let's

--- examples/password_manager/frontend/src/store/vaults.ts
+++ examples/password_manager/frontend/src/store/vaults.ts
@@ -48,7 +48,7 @@ export async function refreshVaults(
 
     const vaultsSharedWithMe = await encryptedMaps.get_accessible_shared_map_names();
 
-    var vaultIds = new Array<[Principal, string]>();
+    let vaultIds = new Array<[Principal, string]>();
 
     for (const vaultNameBytes of vaultsOwnedByMe.Ok) {
         const vaultName = new TextDecoder().decode(Uint8Array.from(vaultNameBytes.inner));
@@ -60,7 +60,7 @@ export async function refreshVaults(
         vaultIds.push([otherOwner, vaultName]);
     }
 
-    var vaults = new Array();
+    let vaults = new Array();
 
     for (const [otherOwner, vaultName] of vaultIds) {
         const result = await encryptedMaps.get_values_for_map(otherOwner, vaultName);
