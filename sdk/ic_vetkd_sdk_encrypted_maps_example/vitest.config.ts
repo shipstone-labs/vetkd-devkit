43144547ac92ebcb1c84c4e4047b7ee84ceff3b4
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-02-28 15:58:22 +0100

    try fixing CI

--- sdk/ic_vetkd_sdk_encrypted_maps_example/vitest.config.ts
+++ sdk/ic_vetkd_sdk_encrypted_maps_example/vitest.config.ts
@@ -1,6 +1,32 @@
 import { defineConfig } from 'vitest/config'
+import path from 'path';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'ic_vetkd_sdk_encrypted_maps',
+            formats: ['es', 'umd'],
+            fileName: (format) => `ic_vetkd_sdk_encrypted_maps.${format}.js`
+        },
+        rollupOptions: {
+            external: [],
+            output: {
+                globals: {}
+            }
+        }
+    },
+    plugins: [
+        wasm(),
+        topLevelAwait()
+    ],
+    esbuild: {
+        supported: {
+            'top-level-await': true //browsers can handle top-level-await features
+        },
+    },
     test: {
         environment: "happy-dom",
         setupFiles: ['test/setup.ts'],
--- sdk/ic_vetkd_sdk_key_manager_example/vitest.config.ts
+++ sdk/ic_vetkd_sdk_key_manager_example/vitest.config.ts
@@ -1,6 +1,32 @@
 import { defineConfig } from 'vitest/config'
+import path from 'path';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'ic_vetkd_sdk_key_manager',
+            formats: ['es'],
+            fileName: (format) => `ic_vetkd_sdk_key_manager.${format}.js`
+        },
+        rollupOptions: {
+            external: [],
+            output: {
+                globals: {}
+            }
+        }
+    },
+    plugins: [
+        wasm(),
+        topLevelAwait()
+    ],
+    esbuild: {
+        supported: {
+            'top-level-await': true //browsers can handle top-level-await features
+        },
+    },
     test: {
         testTimeout: 60000
     }
