3384c06f9e900a681d9596ee9fb6625eea4b65cc
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-02-28 18:07:52 +0100

    try to fix CI once again

--- sdk/ic_vetkd_sdk_key_manager_example/jest.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*/ic_vetkd_cdk_utils_bg\\.wasm\\.js)$': '$1',
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    }
-};
-
-module.exports = config;
